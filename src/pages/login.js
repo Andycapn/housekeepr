9f5591a47c3de692e9fa5a923c3554ed955cf102
andycapn <aundreycap@gmail.com>
2020-05-18 03:24:40 +0200

    Added login page markup

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,7 +5,20 @@ const Login = () => {
 
     return (
         <PageLayout>
-            <h1>Login Page</h1>
+            <h1>HouseKeepr</h1>
+            <h2>Chaminuka Lodge</h2>
+            <h3>Login</h3>
+            <form action="">
+                <label htmlFor="first-name">
+                    Username/Email
+                    <input name="email/username" type="email"/>
+                </label>
+                <label htmlFor="first-name">
+                    Password
+                    <input name="first-name" type="password"/>
+                </label>
+                <input type="submit"/>
+            </form>
         </PageLayout>
     );
 };
