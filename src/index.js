e91de4fea9d878142d69dcca66f64b06ed2ff843
andycapn <aundreycap@gmail.com>
2020-05-18 23:07:38 +0200

    Began work on login functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import {render} from 'react-dom';
 import {BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/@blueprintjs/core/lib/css/blueprint.css"
+import "../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css"
 
 render(
     <React.StrictMode>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,37 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { PageLayout, Title, BodyText } from "../components/styledelements";
-import "../Stylesheets/login.css"
-import { Jumbotron, Form, Button }  from "react-bootstrap";
-import {css} from "@emotion/core"
+import "../Stylesheets/login.css";
+import { Jumbotron, Form } from "react-bootstrap";
+import { Button, Callout } from "@blueprintjs/core";
+import Logo from "../Images/icons/cleaning.svg";
 
 const Login = () => {
-    const submitForm = (e) => {
-        e.preventDefault()
-        alert("Logging In")
-    }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    return (
-        <PageLayout className={`login-main`}>
-            <Title style={{color: "white", textAlign: "center", marginTop: "8vh"}}>HouseKeepr</Title>
-            <BodyText style={{color: "white", letterSpacing: "1.68px", textAlign: "center"}}>Chaminuka Lodge</BodyText>
-            <BodyText style={{color: "white", letterSpacing: "1.68px", textAlign: "center"}}>Login</BodyText>
-            <Jumbotron>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" className="submit-btn" onClick={e => submitForm(e)}>
-                        Submit
-                    </Button>
-                </Form>
-            </Jumbotron>
-        </PageLayout>
-    );
+  const submitForm = (e) => {
+    e.preventDefault();
+    if (email === "" || password === "") {
+      setError("All fields are required");
+    }
+  };
+  return (
+    <PageLayout className={`login-main`}>
+      <img src={Logo} className="logo" alt="" />
+      <Title style={{ color: "white", textAlign: "center" }}>HouseKeepr</Title>
+      <BodyText style={{ color: "white", textAlign: "center" }}>
+        Chaminuka Lodge
+      </BodyText>
+      <BodyText style={{ color: "white", textAlign: "center" }}>Login</BodyText>
+      <Jumbotron className={`form-jumbo`}>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          {error !== "" ? (
+            <Callout style={{ marginBottom: "20px" }} intent="danger">
+              {error}
+            </Callout>
+          ) : null}
+          <Button
+            intent="primary"
+            outlined
+            minimal
+            fill
+            type="submit"
+            className="submit-btn"
+            onClick={(e) => submitForm(e)}
+            rightIcon={`log-in`}
+          >
+            Log in
+          </Button>
+        </Form>
+      </Jumbotron>
+    </PageLayout>
+  );
 };
 
-export default Login
+export default Login;
