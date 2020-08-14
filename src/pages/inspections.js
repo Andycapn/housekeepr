7e68738307406f0b07183e419a11b320e495ff27
andycapn <aundreycap@gmail.com>
2020-08-15 00:37:55 +0200

    Added loading spinner while waiting for data

--- src/pages/inspections.js
+++ src/pages/inspections.js
@@ -4,6 +4,14 @@ import { MainDiv } from "../components/MyStyledComonents";
 import { Card, HTMLTable } from "@blueprintjs/core";
 import axios from "axios";
 import { useCookies } from "react-cookie";
+import { ClipLoader } from "react-spinners";
+import { css } from "@emotion/core";
+
+const spinnerStyling = css`
+  margin: 25% auto;
+  position: relative;
+  top: 45%;
+`;
 
 // URI Encode data
 const encode = (data) => {
@@ -17,49 +25,65 @@ const Inspections = () => {
 
   const [inspectionPageState, setInspectionPageState] = useState({
     inspections: [cookies.housekeepr],
+    loading: true,
   });
 
   useEffect(() => {
-    axios
-      .post("http://localhost:3000/inspections/get-inspections", encode({ token: cookies.housekeepr }))
-      .then((response) => {
-        console.log(response.data);
-        setInspectionPageState({ inspections: response.data.result });
-      });
+    if (inspectionPageState.loading) {
+      axios
+        .post("http://localhost:3000/inspections/get-inspections", encode({ token: cookies.housekeepr }))
+        .then((response) => {
+          console.log(response.data);
+          setInspectionPageState({ inspections: response.data.result, loading: false });
+        });
+    }
   }, [cookies.housekeepr]);
 
   return (
     <Layout>
-      <MainDiv>
-        <Card>
-          <h1>Inspections</h1>
-          <hr />
-          <HTMLTable style={{ width: "100%" }} bordered>
-            <thead>
-              <tr>
-                <th>Inspection ID</th>
-                <th>Inspected By</th>
-                <th>Conducted On</th>
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              {!inspectionPageState.inspections ? (
-                <h3 style={{ textAlign: "center" }}>No Inspections Yet</h3>
-              ) : (
-                inspectionPageState.inspections.map((element, index) => (
-                  <tr key={index}>
-                    <td>{inspectionPageState.inspections[index].inspection_id}</td>
-                    <td>{inspectionPageState.inspections[index].first_name}</td>
-                    <td>{inspectionPageState.inspections[index].date}</td>
-                    <td>null</td>
-                  </tr>
-                ))
-              )}
-            </tbody>
-          </HTMLTable>
-        </Card>
-      </MainDiv>
+      {inspectionPageState.loading ? (
+        <div
+          style={{
+            textAlign: "center",
+            display: "flex",
+            flexDirection: "center",
+            justifyContent: "center",
+          }}
+        >
+          <ClipLoader css={spinnerStyling} />
+        </div>
+      ) : (
+        <MainDiv>
+          <Card>
+            <h1>Inspections</h1>
+            <hr />
+            <HTMLTable style={{ width: "100%" }} bordered>
+              <thead>
+                <tr>
+                  <th>Inspection ID</th>
+                  <th>Inspected By</th>
+                  <th>Conducted On</th>
+                  <th>Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                {!inspectionPageState.inspections ? (
+                  <h3 style={{ textAlign: "center" }}>No Inspections Yet</h3>
+                ) : (
+                  inspectionPageState.inspections.map((element, index) => (
+                    <tr key={index}>
+                      <td>{inspectionPageState.inspections[index].inspection_id}</td>
+                      <td>{inspectionPageState.inspections[index].first_name}</td>
+                      <td>{inspectionPageState.inspections[index].date}</td>
+                      <td>null</td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </HTMLTable>
+          </Card>
+        </MainDiv>
+      )}
     </Layout>
   );
 };
