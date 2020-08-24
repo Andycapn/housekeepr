eec63d98a9d6486b74821d6e7f124eb137250a72
andycapn <aundreycap@gmail.com>
2020-08-24 12:40:37 +0200

    Wrapped Visable elements in a card component

--- src/pages/Dashboard/dashboard.js
+++ src/pages/Dashboard/dashboard.js
@@ -3,6 +3,8 @@ import Layout from "../../components/layout";
 import Context from "../../store/context";
 import { MainDiv } from "../../components/MyStyledComonents";
 import "./dashboard.css";
+import { Card } from "@blueprintjs/core";
+import { Title } from "../../components/styledelements";
 
 function Dashboard() {
   const { state } = useContext(Context);
@@ -13,8 +15,10 @@ function Dashboard() {
         <main className="dashboard-main">
           <section className="main-app-section">
             <MainDiv>
-              <h1>Hi {state.first_name}, You have conducted N inspections today</h1>
-              <pre>{JSON.stringify(state)}</pre>
+              <Card>
+                <Title>Hi {state.first_name}, You have conducted N inspections today</Title>
+                <pre>{JSON.stringify(state)}</pre>
+              </Card>
             </MainDiv>
           </section>
         </main>
