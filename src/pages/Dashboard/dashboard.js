2e22a466c7b1be60a4359903d0e8e59500d94267
andycapn <aundreycap@gmail.com>
2020-09-10 10:38:19 +0200

    Added Dynamic Count for completed inspections on dashboard

--- src/pages/Dashboard/dashboard.js
+++ src/pages/Dashboard/dashboard.js
@@ -1,12 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Layout from "../../components/layout";
 import Context from "../../store/context";
 import { MainDiv } from "../../components/MyStyledComonents";
 import "./dashboard.css";
 import { Card } from "@blueprintjs/core";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import { css } from "@emotion/core";
+import { ClipLoader } from "react-spinners";
 
-function Dashboard() {
+const spinnerStyling = css`
+  margin: 25% auto;
+  position: relative;
+  top: 45%;
+`;
+
+const Dashboard = () => {
   const { state } = useContext(Context);
+  const [cookies] = useCookies();
+  const [dashboardState, setDashboardState] = useState({ loading: true, count: 0 });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/inspections/getCount", {
+        headers: { authorization: cookies.housekeepr },
+      })
+      .then((response) => {
+        setDashboardState({ loading: false, count: response.data.count });
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  if (dashboardState.loading) {
+    return (
+      <Layout>
+        <div style={{ textAlign: "center" }}>
+          <ClipLoader css={spinnerStyling} color={"#41b7fc"} />
+        </div>
+      </Layout>
+    );
+  }
 
   return (
     <Layout>
@@ -15,10 +48,10 @@ function Dashboard() {
           <section className="main-app-section">
             <MainDiv>
               <Card>
-                <h1 className="bp3-heading" style={{ color: "#05B2DC" }}>
-                  Hi {state.first_name}, You have conducted N inspections today
+                <h1 className="bp3-heading" style={{ color: "#05B2DC", textAlign: "center" }}>
+                  Hi {state.first_name}, You have conducted {dashboardState.count}{" "}
+                  {dashboardState.count === 1 ? "inspection" : "inspections"} so far.
                 </h1>
-                <pre>{JSON.stringify(state)}</pre>
               </Card>
             </MainDiv>
           </section>
@@ -26,6 +59,6 @@ function Dashboard() {
       </>
     </Layout>
   );
-}
+};
 
 export default Dashboard;
